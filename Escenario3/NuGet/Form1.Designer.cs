5543979f1005390ea75587e71861d429652148cc
Peters <peterscristell18@gmail.com>
2026-09-21 23:44:16 -0500

    subir escenario

---
+++ Escenario3/NuGet/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace NuGet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnError = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(156, 53);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(194, 25);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Escenario 3 - Serilog";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(189, 114);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(124, 38);
+            this.btnRegistrar.TabIndex = 1;
+            this.btnRegistrar.Text = "Registrar evento";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnError
+            // 
+            this.btnError.Location = new System.Drawing.Point(189, 171);
+            this.btnError.Name = "btnError";
+            this.btnError.Size = new System.Drawing.Size(124, 38);
+            this.btnError.TabIndex = 2;
+            this.btnError.Text = "Generar error";
+            this.btnError.UseVisualStyleBackColor = true;
+            this.btnError.Click += new System.EventHandler(this.btnError_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 292);
+            this.Controls.Add(this.btnError);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnError;
+    }
+}
+
---
+++ Escenario3/NuGet/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Serilog;
+
+namespace NuGet
+{
+    public partial class Form1 : Form
+    {
+        private static readonly ILogger logger = new LoggerConfiguration()
+            .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            logger.Information("La aplicación se inició correctamente.");
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            logger.Information("El usuario presionó el botón Registrar evento.");
+            MessageBox.Show("Evento registrado correctamente.");
+        }
+
+        private void btnError_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = 10;
+                int resultado = numero / 0;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Ocurrió un error al realizar una operación.");
+                MessageBox.Show("Se produjo un error. El evento fue registrado.");
+            }
+        }
+    }
+}
